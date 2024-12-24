47faf37c8941ce1e7d5cf275ff8d6e3fc798c394
WeearwanK <weerawanpoint@gmail.com>
2024-12-24 15:35:59 +0700

    completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,24 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    int enter = 0;
+    while (enter < 3) {
+        getchar();
+        enter++;
+    }
+    
+    srand(time(0));
+    int grade = rand() % 9;
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "You will get " << grades[grade] << " in this 261102." << endl;
+
+    return 0;
+}
